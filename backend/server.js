4f3a5ac0f0ffae4d16e5ecccc39bc934a0d37937
Naeem-khndkar55 <naeemkhandakar5@gmail.com>
2025-03-17 21:52:15 +0600

    Backend initialized and database connected

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+const fileUpload = require("express-fileupload");
+const connectDB = require("./config/db");
+require("dotenv").config();
+
+const app = express();
+
+// Connect to MongoDB
+connectDB();
+
+// // Middleware
+// app.use(cors());
+// app.use(express.json());
+// app.use(fileUpload({ useTempFiles: true }));
+
+// // Routes
+// app.use("/api/auth", require("./routes/authRoutes"));
+// app.use("/api/vehicles", require("./routes/vehicleRoutes"));
+// app.use("/api/moderators", require("./routes/moderatorRoutes"));
+
+// Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
